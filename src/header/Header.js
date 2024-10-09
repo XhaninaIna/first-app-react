6af86aa272be5275bc9e1cc9b6ecb141a5a22d17
Xhanina <166377712+XhaninaIna@users.noreply.github.com>
2024-10-09 16:48:17 +0200

    first commit

---
+++ src/header/Header.js
@@ -0,0 +1,15 @@
+
+export default function Header(){
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    )
+}
+function Logo(){
+    return(
+        <div className="logo">
+            <img src="images/logo.jpg" alt="logo" />
+        </div>
+    )
+}
